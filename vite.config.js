f95ddd4d4663432cd93f0ce33a3f5b760e0043ea
SzymCode <szymon.radomski@yahoo.com>
2024-08-18 22:36:58 +0200

    Correction for deploy

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,13 @@
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  build: {
-    outDir: "public",
-    assetsDir: "",
-    sourcemap: false,
-    rollupOptions: {
-      input: path.resolve(__dirname, "src/main.ts"),
-      output: {
-        entryFileNames: "[name].js",
-        chunkFileNames: "[name].js",
-        assetFileNames: "[name].[ext]",
-        sourcemapExcludeSources: true
-
-      },
-    },
-    cssCodeSplit: false,
-  },
-  define: {
-    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
-  },
-  base: "/public",
-  publicDir: "../../views",
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+    ]
+  }
 });
